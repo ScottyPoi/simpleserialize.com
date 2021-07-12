3b70cad69ba861ab8afcf1b2e6461a14c7ade23f
ScottyPoi <kyzambo@gmail.com>
2021-07-12 17:29:14 -0600

    add tab and visualizer component

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Serialize from "./Serialize";
+import Visualize from "./Visualize";
 
-type Props = {
-
-};
+type Props = Record<string, unknown>;
 
 type State = {
   serializeModeOn: boolean;
+  visualizeModeOn: boolean;
 };
 
 export default class Tabs extends React.Component<Props, State> {
@@ -14,19 +14,24 @@ export default class Tabs extends React.Component<Props, State> {
     super(props);
     this.state = {
       serializeModeOn: true,
+      visualizeModeOn: false,
     };
   }
 
   showSerialize(): void {
-    this.setState({serializeModeOn: true});
+    this.setState({serializeModeOn: true, visualizeModeOn: false});
   }
 
   showDeserialize(): void {
-    this.setState({serializeModeOn: false});
+    this.setState({serializeModeOn: false, visualizeModeOn: false});
+  }
+
+  showVisualize(): void {
+    this.setState({serializeModeOn: false, visualizeModeOn: true});
   }
 
   render(): JSX.Element {
-    const {serializeModeOn} = this.state;
+    const {serializeModeOn, visualizeModeOn} = this.state;
     return (
       <>
         <div className="tabs is-centered">
@@ -34,14 +39,34 @@ export default class Tabs extends React.Component<Props, State> {
             <li
               className={serializeModeOn ? "is-active" : "is-inactive"}
               onClick={() => this.showSerialize()}
-            ><a>Serialize</a></li>
+            >
+              <a>Serialize</a>
+            </li>
             <li
-              className={serializeModeOn ? "is-inactive" : "is-active"}
+              className={
+                serializeModeOn
+                  ? "is-inactive"
+                  : visualizeModeOn
+                    ? "is-inactive"
+                    : "is-active"
+              }
               onClick={() => this.showDeserialize()}
-            ><a>Deserialize</a></li>
+            >
+              <a>Deserialize</a>
+            </li>
+            <li
+              className={visualizeModeOn ? "is-active" : "is-inactive"}
+              onClick={() => this.showVisualize()}
+            >
+              <a>Visualize</a>
+            </li>
           </ul>
         </div>
-        <Serialize serializeModeOn={serializeModeOn} />
+        {visualizeModeOn ? (
+          <Visualize visualizeModeOn={true} />
+        ) : (
+          <Serialize serializeModeOn={serializeModeOn} />
+        )}
       </>
     );
   }
